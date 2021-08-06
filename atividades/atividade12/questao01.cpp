fea9b88e01abc660e1be7955145335e3766d9ac7
Alcides Ribeiro <alcidesribeirosampaioneto@gmail.com>
2021-08-05 22:14:55 -0300

    a12q01 corrigindo distribuição de tarefas

--- atividades/atividade12/questao01.cpp
+++ atividades/atividade12/questao01.cpp
@@ -1,44 +1,48 @@
 #include <bits/stdc++.h>
 #include <mpi.h>
 
-#define ROOT 0
-
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
     int id, np;
-    const int N = atoi(argv[3]);
-    vector<double> arr1(N,atof(argv[1])),
-                   arr2(N,atof(argv[2]));
-    double res = 0;
+    const int N = atoi(argv[3]), ROOT = 0;
+    vector<double> arr1(N,atof(argv[1])), arr2(N,atof(argv[2]));
 
     MPI_Init(nullptr, nullptr);
     MPI_Comm_rank(MPI_COMM_WORLD, &id);
     MPI_Comm_size(MPI_COMM_WORLD, &np);
 
-    double begin_time, acc;
-    int size, gap;
+    int size, range, i;
+    double res, ans, begin;
+    vector<double> buf1, buf2;
 
     if (id == ROOT) {
-        begin_time = MPI_Wtime();
         size = N;
+        begin = MPI_Wtime();
     }
 
     MPI_Bcast(&size, 1, MPI_INT, ROOT, MPI_COMM_WORLD);
-    
-    gap = size/np;
-    acc = 0;
 
-    for (int i=id*gap; i < id*gap+gap; i++) {
-        acc += arr1[i]*arr2[i];
-    }
+    range = size/np;
+    buf1.resize(range);
+    buf2.resize(range);
 
-    MPI_Reduce(&acc, &res, 1, MPI_DOUBLE, MPI_SUM, ROOT, MPI_COMM_WORLD);    
+    MPI_Scatter(&arr1[0], range, MPI_DOUBLE, &buf1[0], range, MPI_DOUBLE, ROOT, MPI_COMM_WORLD);
+    MPI_Scatter(&arr2[0], range, MPI_DOUBLE, &buf2[0], range, MPI_DOUBLE, ROOT, MPI_COMM_WORLD);
+
+    res = 0.0;
+
+    for (i=0; i<range; i++) res += buf1[i]*buf2[i];
+
+    ans = 0.0;
+
+    MPI_Reduce(&res, &ans, 1, MPI_DOUBLE, MPI_SUM, ROOT, MPI_COMM_WORLD);
 
     if (id == ROOT) {
-        cout << np << "," << MPI_Wtime() - begin_time << endl;
-    }
+        //cout << ans << endl;
+        cout << MPI_Wtime()-begin << endl;
+    } 
 
     MPI_Finalize();
 
