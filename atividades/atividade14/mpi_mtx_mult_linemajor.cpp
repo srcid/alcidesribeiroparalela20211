d60b49aa1b363f4f4fe71537a21439ef35f36c5a
Alcides Ribeiro <alcidesribeirosampaioneto@gmail.com>
2021-08-18 10:25:53 -0300

    alterações atividade14

--- atividades/atividade14/mpi_mtx_mult_linemajor.cpp
+++ atividades/atividade14/mpi_mtx_mult_linemajor.cpp
@@ -2,22 +2,10 @@
 #include <vector>
 #include <mpi.h>
 
-#define ECHORES 0
-
 using namespace std;
 
 constexpr int sqr(int a) {
     return a*a;
-}
-
-template <class T>
-inline void transverse(vector<T>& arr, const int m) {
-    int i, j;
-    for (i=0; i<m; i++) {
-        for (j=0; j<m; j++) {
-            swap(arr[i*m+j], arr[j*m+i]);
-        }
-    }
 }
 
 template <class T>
@@ -36,70 +24,55 @@ vector<T> multm(vector<T>& ma, vector<T>& mb, const int NROWS, const int N) {
     return mc;
 }
 
-template <class T>
-void parr(vector<T>& arr) {
-    cout << '[';
-    if (!arr.empty()) {
-        cout << arr.front();
-        for_each(arr.begin()+1, arr.end(), [](auto e) {
-            cout << ',' << e;
-        });
-    }
-    cout << ']' << endl;
-}
-
-template <class T>
-void print_mtx_array(vector<T>& arr, const int N) {
-    int i = 0;
-    for (auto n : arr) {
-        cout << n << " ";
-        i = (i < N-1 ? i+1 : 0);
-        if (!i) cout << endl;
-    }
-    cout << endl;
-}
-
-
 int main(int argc, char *argv[])
 {
     int rank, size;
     const int msize = atoi(argv[1]);
-
-    vector<double> mtxa(sqr(msize), 1.0),
-                   mtxb(sqr(msize), 1.0),
-                   mtxc(sqr(msize));
+    vector<double> mtxa, mtxb, mtxc;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     
     double wtimebeg;
+    
+    if (!rank) wtimebeg = MPI_Wtime();
 
-    const int RANGE = sqr(msize)/size,
-              LINES = RANGE/msize;
+    const int RANGE = sqr(msize)/size, /* Número de elementos a serem processados */
+              LINES = RANGE/msize;     /* Número de linhas que cada processo pegou */
 
-    vector<double> abuf(RANGE),
-                   bbuf(sqr(msize)),
-                   res;
+    vector<double> abuf, res;
 
-    if (!rank) {
-        wtimebeg = MPI_Wtime();
-        bbuf.insert(bbuf.begin(), mtxb.begin(), mtxb.end());
-    }
+    abuf.resize(RANGE);
 
+    /* Apenas 0 aloca a matriz A a inteiramente, e, depois de enviá-la, a desaloca */
+    if (!rank) mtxa.insert(mtxa.begin(), sqr(msize), 1.0);
+    
     MPI_Scatter(&mtxa[0], RANGE, MPI_DOUBLE, &abuf[0], RANGE, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-    MPI_Bcast(&bbuf[0], sqr(msize), MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    
+    if (!rank) mtxa.clear();
 
-    res = multm(abuf, bbuf, LINES, msize);
+    /* 0 carrega a matriz B e a copia pelos demais processos */
+    if (!rank) mtxb.insert(mtxb.begin(), sqr(msize), 1.0);
+    else mtxb.reserve(sqr(msize));
+    
+    MPI_Bcast(&mtxb[0], sqr(msize), MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    res = multm(abuf, mtxb, LINES, msize);
+
+    mtxb.clear();
+
+    if (!rank) mtxc.resize(sqr(msize));
     
     MPI_Gather(&res[0], RANGE, MPI_DOUBLE, &mtxc[0], RANGE, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
+    /* A barreira é usada para assegurar que 0 não terminará antes dos outros impactando
+     * a medida de tempo para baixo 
+     */
     MPI_Barrier(MPI_COMM_WORLD);
 
-    if (!rank) {
-        if (ECHORES) print_mtx_array(mtxc, msize);
-        cout << MPI_Wtime() - wtimebeg << endl;
-    }
+    if (!rank) cout << MPI_Wtime() - wtimebeg << endl;
 
     MPI_Finalize();
 
