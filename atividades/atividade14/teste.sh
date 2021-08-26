8bfddad9ac528d029b236f853c548eee31aab9d5
Alcides Ribeiro <alcidesribeirosampaioneto@gmail.com>
2021-08-26 11:05:03 -0300

    corrigindo o comando sed e liberando as variaveis usadas

--- atividades/atividade14/teste.sh
+++ atividades/atividade14/teste.sh
@@ -8,9 +8,11 @@ do
     echo "1x8,$s,$i,$(mpirun -np $i ./a.out $s)" >> res.txt
 done
 
-sed 's/8$/4' "/home/hostfile" > "hostfile"
+sed 's/8$/4/' "/home/hostfile" > "hostfile"
 
 for i in 2 4 8
 do
     echo "2x4,$s,$i,$(mpirun --hostfile hostfile -np $i ./a.out $s)" >> res.txt
 done
+
+unset s i
